3fa4509808f69287a40c3e2b31df9be4104a9a31
samueljim <samueljim@msn.com>
2018-07-27 21:54:34 +1000

    404 screen and sw update

--- sw-config.js
+++ sw-config.js
@@ -1,6 +1,9 @@
 module.exports = {
     staticFileGlobs: [
         'public/style.css',
+        'public/main.js',
+        'public/404.html',
+        'public/pwa.js',
     ],
     importScripts: ['/offline.js'],
     stripPrefix: 'public'
