ca79370b65428d5cc16ae31f5523831480b50e65
samueljim <samueljim@msn.com>
2018-07-28 12:55:01 +1000

    clean up

--- public/main.js
+++ public/main.js
@@ -4,11 +4,6 @@ $(document).ready(function () {
         console.log(gnAvailable);
         var doAvailable = gn.isAvailable(GyroNorm.DEVICE_ORIENTATION);
         console.log(doAvailable);
-        // gn.init().then(function () {
-        //     gn.start(function (data) {
-        //         console.log(data);
-        //     });
-        // }).catch(function (e) {});
     }
 
     var gn = new GyroNorm();
